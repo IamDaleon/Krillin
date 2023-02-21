257c4c32e350aee4dba4e07d8cc37a505e5bf89c
iamdaleon <daleon86@gmail.com>
2023-02-21 16:30:27 -0600

    Learning Wordpress theme development

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+ <html <?php language_attributes(); ?>>
+ <head>
+    <meta charset="<?php bloginfo('charaset');?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+ <body <?php body_class();?>
+
+    <h1>Hello World</h1>
+    <header> <secton clas='top-bar'>
+        <div class
+ </body>
+ </html>
