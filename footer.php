c853a117ca186c8194b7b68584ee9cb6a030d31e
iamdaleon <daleon86@gmail.com>
2023-03-01 16:57:20 -0600

    brush up on the template

--- footer.php
+++ footer.php
@@ -1,63 +1,48 @@
 <footer class="site-footer">
-   <div class='centering'>
-       <h3 class='cortex'>Disclaimer</h3>
-       <h6 class='reducer'>Information and statements regarding our products have not been evaluated by the Food and Drug Administration<br /> and are not intended to diagnose, treat, cure or prevent any disease or health condition. Our product is not recommended for women who are pregnant, women who are nursing, or children. <br />NOT FOR SALE TO MINORS! <br />18+ ONLY. | (21+ IN TENNESSEE).</h6>
-   </div>
-   <!-- background table start
-<table width="100%" bgcolor="#f7f7f7" cellpadding="0" cellspacing="0" border="0" id="background_table">
-  <tbody>
-    <tr>
-      <td>
-<!-- end of background table start -->
-
-<!-- 3 columns -->
-
-            <!-- <table width="900" cellpadding="0" cellspacing="0" border="0" align="center" class="body_table">
-                <tbody>
-                    <tr>
-                        <td width="33%" bgcolor="#CAECAE" class="column">Links</td>
-                        <td width="335" bgcolor="#F6D672" class="column">
-                            <h5>Get Exclusive Offers!</h5>
-                        </td>
-                        <td width="300" bgcolor="#B0E3F0" class="column">Column 3</td>
-                    </tr>
-                </tbody>
-            </table> -->
-<!-- end of 3 columns -->
-
-<!-- footer -->
-            <!-- <table width="600" cellpadding="0" cellspacing="0" border="0" align="center" class="body_table">
-              <tbody>
-                <tr>
-                  <td width="100%"><table width="600" cellpadding="0" cellspacing="0" border="0" align="center" class="body_table">
-                      <tbody>
-                        <!-- Spacing -->
-                        <tr>
-                          <td height="20" style="font-size:1px; line-height:1px; mso-line-height-rule: exactly;">&nbsp;</td>
-                        </tr>
-                        <!-- Spacing -->
-                      </tbody>
-                    </table></td>
-                </tr>
-              </tbody>
-            </table> -->
-<!-- end of footer -->
-
-<!-- end of background table-->
-        </td>
-    </tr>
-  </tbody>
-</table> -->
-<div class="container">
-    <div class="copyright">
-        <p class='smallertx'>All Copyright OPMS - All Rights Reserved. </p>
+<div class="row justify-content-lg-center">
+    <div class="col col-lg-2">
+      <ul class="simpler">
+        <li><a href="/"> HOME</a></li>
+        <li>PRODUCTS</li>
+        <li>SITEMAP</li>
+        <li>TERMS & CONDITION</li>
+        <li>PRIVACY</li>
+        <li>CONTACT</li>
+      </ul>
+    <span class="footer-hider"><?php wp_nav_menu( array('theme_location' => 'krillin_footer-menu', 'depth' =>1 )); ?></span>
     </div>
-        <nav class="footer-menu smallertx">
-            <?php wp_nav_menu( array('theme_location' => 'krillin_footer-menu', 'depth' =>1 )); ?>
-        </nav>
+    <div class="vl col-lg-auto"></div>
+    <div class="col- col-lg-6 text-center">
+    <h3 class="text-center col-white">Get Exclusive Offers</h3>
+        <span>Be the first to know about exclusive offers and product updates.</span>
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted txt-reducer">We'll never share your email with anyone else.</small>
+          </div>
+          <br />
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-        </footer>
+    <div class="vl col-lg-auto"></div>
+    
+    <div class="col col-lg-2 text-center">
+    <a class="navbar-brand" href="/"><img src='<?php echo get_template_directory_uri(); ?>/assets/images/OPMS.webp' width='65vw' height='65vh'></a>
+        <p>Optimized Plant Mediated Solutions has been the leader in the Kratom and Kava extract industry since 2010 due to its unique cold water extraction process.</p>
     </div>
+  </div>
+  <br />
+  <hr class="hr-reducer" />
+  <div class="container">
+      <div class="copyright">
+          <p class='smallertx footer-bottom'>All Copyright OPMS - All Rights Reserved. </p>
+      </div>
+          <nav class="footer-menu smallertx">
+              <?php wp_nav_menu( array('theme_location' => 'krillin_footer-menu', 'depth' =>1 )); ?>
+          </nav>
+      </div>
+          </footer>
+      </div>
     <?php wp_footer(); ?>
-    </body>
- </html>
+</div>
--- head.php
+++ head.php
@@ -1,14 +1,21 @@
-  <div class="navvie">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="navvie">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src='<?php echo get_template_directory_uri(); ?>/assets/images/OPMS.webp' width='65vw' height='65vh'></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="row">
+    <div class="col-sm">
+    <a class="navbar-brand" href="/"><img src='<?php echo get_template_directory_uri(); ?>/assets/images/OPMS.webp' width='65vw' height='65vh'></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-            </button>
+            </button>  
+  </div>
+    <div class="col-sm">
+      ...
+    </div>
+    <div class="col-sm"> 
             <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">LEARN</a>
+                <a class="nav-link active" aria-current="page" href="/about">LEARN</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -33,6 +40,7 @@
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form> -->
             </div>
-        </div>
-    </nav>
+</div>
   </div>
+</div>
+</div>
