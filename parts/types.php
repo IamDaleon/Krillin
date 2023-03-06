a7a62693dd4e4b39b0e97d79a430ad7f483c224b
iamdaleon <daleon86@gmail.com>
2023-03-06 16:01:47 -0600

    Daily due

--- parts/types.php
+++ parts/types.php
@@ -20,3 +20,94 @@
     </div>
   </div>
   </div>
+<!-- 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Components</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head> -->
+
+<div class="buttonup">
+    <main class="main">
+        <section class="card-lg__container px-10">
+         <p class="card__text-sm">O.P.M.S. - Capsules</p>
+          <h1 class="card__text-lg text-center">CAPSULES</h1>
+          <p class="card__text-md">Add it into you daily pill intake.</p>
+          <a href="#" class="card__btn">View More!</a>
+        </section>
+      
+      
+        <section class="card-sm__container">
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/caps/OPMS-Gold-5ct-reduced-768x1156-1.jpg'); background-position: center center;">
+            <!-- <p class="card__text-sm">Tanifa - Spain</p>
+            <h1 class="card__text-lg">OPK KAVA</h1> -->
+          </section>
+          <!--- END: CARD --->
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/Gold-Liquid.webp'); background-position: center center;">
+          </section>
+          <!--- END: CARD --->
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('./assets/imgs/caps/OPMS-Black-5ct-e1616000475735.png');  background-position: center center;">
+          </section>
+          <!--- END: CARD --->
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('./assets/imgs/caps/OPMS-box-group-variety-2-e1614802988161-300x174.png');  background-position: center center;">
+          </section>
+          <!--- END: CARD --->
+         
+      
+    </main>
+    <main class="main">
+        <section class="card-lg__container px-10">
+          <!-- <p class="card__text-sm">O.P.M.S. - Capsules</p> -->
+          <h1 class="card__text-lg text-center">Liquids</h1>
+          <p class="card__text-md">On the go... Grab a bottle!</p>
+          <a href="#" class="card__btn">View More!</a>
+        </section>
+      
+      
+        <section class="card-sm__container">
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image:url('./assets/imgs/liq/opk2-768x529.png');  background-position: center center;">
+          </section>
+          <!--- END: CARD --->
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('./assets/imgs/liq/New-OPMS-Gold-Liquid-Bottle-1024x1024.webp'); background-position: center center;">
+          </section>
+          <!--- END: CARD --->
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('./assets/imgs/liq/large-OPMS-Black-Liquid-Vile-768x768.png');  background-position: center center;"
+          <!--- END: CARD --->
+         
+      
+    </main>
+
+    <main class="main">
+        <section class="card-lg__container">
+          <!-- <p class="card__text-sm">O.P.M.S. - Capsules</p> -->
+          <h1 class="card__text-lg text-center">Powder</h1>
+          <p class="card__text-md">Add to your Morning Shakes!</p>
+          <a href="#" class="card__btn">View More!</a>
+        </section>
+      
+      
+        <section class="card-sm__container singular">
+          
+      
+          <!--- START: CARD --->
+          <section class="card-sm" style="background-image: url('./assets/imgs/Powder/opk-group.webp');  background-position: center center;">
+          </section>
+          <!--- END: CARD --->      
+    </main>
+  </div>
