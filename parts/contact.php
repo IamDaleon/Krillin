498b264543fb695dc2aa34f3c3bb6fee192088db
iamdaleon <daleon86@gmail.com>
2023-03-09 18:07:28 -0600

    Final for the day!

--- parts/contact.php
+++ parts/contact.php
@@ -1,3 +1,30 @@
 <div class="container containersection">
     <h2 class="cntitler">Contact us!</h2>
+<hr class="condenser"/>
+  <form action="action_page.php">
+
+    <div class="row">
+        <div class="col">
+            <label for="fname" class="fnametitler">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+        </div>
+        <div class="col">
+            <label for="lname" class="fnametitler">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        </div>
+    </div>
+
+    <label for="country" class="fnametitler">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject" class="fnametitler">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:450px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
 </div>
--- parts/locator.php
+++ parts/locator.php
@@ -1,3 +1,12 @@
 <div class="container locatorsection">
     <h1 class="lrtitler">Locator</h1>
+
+
+    <div class="card bg-dark text-white">
+  <img class="card-img filter" src="https://images.unsplash.com/photo-1594935975218-a3596da034a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Card image">
+  <div class="card-img-overlay">
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
 </div>
